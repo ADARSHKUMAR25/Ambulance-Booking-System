56092c20427a81bb5bdfe0bc0875c09c7b4d6a3d
Raj Deepak <rajdeepak02004@gmail.com>
2025-06-25 01:34:52 +0530

    update-0.1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from backend.database import db
-app = None
+from backend.controllers import setup_routes
 
 def create_app():
     app = Flask(__name__)
@@ -8,10 +8,10 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///dbname.sqlite3"
     db.init_app(app)
     app.app_context().push()
+    setup_routes(app)
     return app
 
 app = create_app()
-from backend.controllers import *
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run()
